021ab2ba17e8918c3163f5129a089fe2ebd606e0
vinnykuhnel <89615917+vinnykuhnel@users.noreply.github.com>
2021-10-29 16:25:56 -0500

    Added Raleigh's cases

--- homework4/test_functions.py
+++ homework4/test_functions.py
@@ -1,6 +1,30 @@
 import pytest
 from functions import *
 
+#import file test cases
+def test_openFile_correct():
+    assert open("test.txt")
+
+def test_openFile_data():
+    assert open(4)
+
+def test_openFile_not():
+    assert open("rest.txt")
+
+#dist test cases
+def test_dist_correct():
+    assert dist(2.0, -3.0, 5.0, 1.0) == 5.0
+
+def test_dist_round():
+    assert dist(-3.0, 4.0, 2.0, 1.0) == 6.0
+
+def test_dist_incorrect():
+    assert dist(9.0, 4.0, 3.0, 1.0) == 51.0
+
+def test_dist_data():
+    assert dist("two", 3.0, 6.0, 5.0) == 10.0
+
+
 ## pytest testing function 1: testing correct input
 
 def test_numbers():
